6fccd0c2f51238b0a284ca8723d3a3d556efd0ad
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-14 16:12:33 +0300

    section 8 done fr

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,28 @@
-console.log('ceva')
+console.log("ceva");
+
+
+const weatherForm = document.querySelector("form");
+const search= document.querySelector('input')//asa luam ce informatia si o putem folosi in js
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location=search.value//cu value extragem valoarea din variabila search
+
+    messageOne.textContent='Loading..'
+    messageTwo.textContent=''
+
+    fetch('http://localhost:3000/weather?address='+location+'').then((response) => {
+        response.json().then((data) => {
+          if (data.error) {
+            messageOne.textContent=data.error;
+          } else {
+            messageOne.textContent=data.forecast.oras
+            messageTwo.textContent=data.forecast
+          }
+        });
+      });
+  console.log(location);
+});
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,65 +1,97 @@
-const path=require('path')
+const path = require("path");
 const express = require("express");
-const hbs = require('hbs')
-
+const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geoCode = require("./utils/geocode");
 
 const app = express();
 
 //define paths for Express config
-const publicDirPath=path.join(__dirname,'../public')
-const viewsPath=path.join(__dirname,'../templates/views')
-const partialsPath=path.join(__dirname,'../templates/partials')
-
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //setup handlebars engine and views location
-app.set('views',viewsPath)
-app.set('view engine', 'hbs')
-hbs.registerPartials(partialsPath)
+app.set("views", viewsPath);
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
 
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weather app',
-        name:'Vlad'
-    })
-})
-
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About app',
-        name:'Vlad'
-    })
-})
-
-app.get('/help',(req, res)=>{
-    res.render('help',{
-        title:'Help app',
-        name:'Vlad'
-    })
-})
-
-app.get("/weather", (req, res) => {
-  res.send({ forecast: "Cloudy", location: "Romania" });
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather app",
+    name: "Vlad",
+  });
 });
 
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        name: 'Vlad',
-        errorMessage:'Help article not found'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About app",
+    name: "Vlad",
+  });
+});
 
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        name: 'Vlad',
-        errorMessage:'Page not found'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help app",
+    name: "Vlad",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "you must provide an address",
+    });
+  }
+
+  geoCode(req.query.address, (error, { lat, long, oras } ={}) => {
+    if (error) {
+      return req.send({ error });
+    }
+    forecast(lat, long, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+        res.send({
+          forecast: forecastData,
+          oras,
+          address: req.query.address,
+        });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "you must provide a search term",
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Vlad",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Vlad",
+    errorMessage: "Page not found",
+  });
+});
 
 app.listen(3000, () => {
   console.log("serverul merge pe portul 3000");
